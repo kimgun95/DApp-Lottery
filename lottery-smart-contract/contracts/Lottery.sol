5dfa3a1e0cd5840aaed29d1f09eefbb3f3218057
kimgun95 <harrison1995@naver.com>
2022-08-11 17:26:21 +0900

    간단한 스마트 컨트랙트 만들기

--- lottery-smart-contract/contracts/Lottery.sol
+++ lottery-smart-contract/contracts/Lottery.sol
@@ -2,5 +2,14 @@ pragma solidity >=0.4.22 <0.9.0; // 스마트 컨트랙트 버전
 
 contract Lottery {
 
-    
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function getSomeValue() public pure returns (uint256 value) {
+        return 5;
+    }
+
 }
