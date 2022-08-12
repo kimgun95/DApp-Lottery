f3baedef708941072c9b8dfdb8271b33e2708af1
kimgun95 <harrison1995@naver.com>
2022-08-12 17:55:40 +0900

    bet(베팅)함수 구현

--- lottery-smart-contract/contracts/Lottery.sol
+++ lottery-smart-contract/contracts/Lottery.sol
@@ -22,6 +22,9 @@ contract Lottery {
     uint256 private _head;
     mapping (uint256 => BetInfo) private _bets; // 사람들이 베팅한 정보들을 queue 자료 형으로 저장
 
+    // Event 작성
+    event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
+
     constructor() public {
         owner = msg.sender;
     }
@@ -33,6 +36,25 @@ contract Lottery {
     function getPot() public view returns (uint256 pot) {
         return _pot;
     }
+
+    /**
+    * @dev 베팅을 한다. 유저는 0.005 ETH를 보내야 하고, 베팅용 1 byte 글자를 함께 보낸다.
+    * 큐에 저장된 베팅 정보는 이후 distribute 함수에서 해결된다.
+    * @param challenges 유저가 베팅하는 글자
+    * @return 함수가 잘 수행되었는지 확인하는 bool 값
+     */
+     function bet(byte challenges) public payable returns (bool result) {
+        // Check the proper ether is sent
+        require(msg.value == BET_AMOUNT, "Not enough ETH");
+
+        // Push bet to the queue
+        require(pushBet(challenges), "Fail to add a new Bet Info");
+
+        // emit event
+        emit BET(_tail - 1, msg.sender, msg.value, challenges, block.number + BET_BLOCK_INTERVAL);
+
+        return true;
+     }
 
     // Bet
         // save the bet to the queue
